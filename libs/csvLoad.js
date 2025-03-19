ef3794f415076c5fc74d9af6f20625a01f52f79e
Ironboy <ironboy@users.noreply.github.com>
2025-03-19 07:11:18 +0100

    csv och table

---
+++ libs/csvLoad.js
@@ -0,0 +1,15 @@
+export default async function csvLoad(url, separator = ',') {
+  let text = (await (await fetch(url)).text()).split('\r\n').join('\n').split('\n');
+  console.log(text)
+  let columnNames = text.splice(0, 1)[0].split(separator).map(x => x.trim());
+  let data = [];
+  for (let row of text) {
+    let obj = {}, cnames = columnNames.slice();
+    for (let columnValue of row.split(separator)) {
+      columnValue = isNaN(columnValue) ? columnValue : +columnValue;
+      obj[cnames.shift()] = columnValue;
+    }
+    data.push(obj);
+  }
+  return data;
+}
---
+++ libs/tableFromData.js
@@ -0,0 +1,34 @@
+import addToPage from './addToPage.js'
+
+// note: expecting data to be an array of objects
+export default function tableFromData({
+  data,
+  numberFormatLocale = 'sv-SE',
+  numberFormatOptions = {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  },
+  columnNames
+}) {
+  let nFormat = Intl.NumberFormat(numberFormatLocale, numberFormatOptions);
+  // expect all objects to have the same keys, 
+  // so read the keys from the first element
+  let entries = Object.entries(data[0]);
+  let count = 0;
+  while (columnNames.length) {
+    entries[count++][0] = columnNames.shift();
+  }
+  let html = '<table><thead><tr>';
+  html += entries.map(x => `<th class="`
+    + (isNaN(x[1]) ? '' : 'right') + `">${x[0]}</th>`).join('');
+  html += '</tr>';
+  for (let row of data) {
+    html += '<tr>';
+    for (let columnValue of Object.values(row)) {
+      html += `<td class="`
+        + (isNaN(columnValue) ? '' : 'right') + `">${isNaN(columnValue) ? columnValue : nFormat.format(columnValue)}</td>`;
+    }
+    html += '</tr>';
+  }
+  addToPage(html);
+}
