8fd685c09a022b0e4bffd171d8e4e93392011247
Ironboy <ironboy@users.noreply.github.com>
2025-03-20 07:14:51 +0100

    falste start...

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,33 @@
 import express from 'express';
+import fs from 'fs';
 import path from 'path';
+import { marked } from 'marked';
+import * as cheerio from 'cheerio';
+
+// Read markdown content
+let md = fs.readFileSync(path.join(import.meta.dirname, '..', 'markdown-content', 'content.md'), 'utf-8');
+
+// Create cache folder
+let cacheFolder = path.join(import.meta.dirname, 'cache');
+fs.existsSync(cacheFolder) && fs.rmSync(cacheFolder, { recursive: true, force: true });
+fs.mkdirSync(cacheFolder);
+
+// Create js files for each js fragment in the markdown file
+// and modify html generated from markdown
+let $ = cheerio.load('<div class="cheerio-holder">' + marked.parse(md) + '</div>');
+let i = 1;
+$('code').each(function () {
+  let codeEl = $(this);
+  fs.writeFileSync(path.join(cacheFolder, i + '.js'),
+    `window.divCodeCanvas = document.querySelector('#code-canvas-${i}');\n` +
+    codeEl.text()
+    , 'utf-8');
+  let codeSnippet = codeEl.text();
+  codeEl.parent().replaceWith('<div id="code-canvas-' + i++ + '"></div>');
+});
+let html = $('.cheerio-holder').html();
+console.log(html);
+
 
 // Port to start the web server on
 const port = 3005
---
+++ markdown-content/content.md
@@ -0,0 +1,15 @@
+# Fint
+
+```js
+let a = 1;
+let b = 2;
+console.log(a+b);
+```
+
+Something nice here
+
+```js
+let a = 3;
+let b = 5;
+console.log(a+b);
+```
