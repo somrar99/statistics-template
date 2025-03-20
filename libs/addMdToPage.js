8adfcd61968b7d9c064f2824fd07f3c3d41fe19e
Ironboy <ironboy@users.noreply.github.com>
2025-03-20 12:13:22 +0100

    remove console.log

--- libs/addMdToPage.js
+++ libs/addMdToPage.js
@@ -2,6 +2,5 @@ import addToPage from './addToPage.js';
 
 // add markdown converted to html to page
 export default function addMdToPage(markdown) {
-  console.log(marked.parse(markdown))
   addToPage(marked.parse(markdown));
 }
