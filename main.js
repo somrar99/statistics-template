f732dda76dc1181a4365fb3328d2bd23fcb523d8
Ironboy <ironboy@users.noreply.github.com>
2025-03-24 08:20:07 +0100

    working

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
-import makeChartFriendly from './libs/makeChartFriendly.js'; './libs/makeChartFriendly.js';
-import csvLoad from './libs/csvLoad.js';
-import tableFromData from './libs/tableFromData.js';
+import jload from './libs/jload.js';
+import addToPage from './libs/addToPage.js'
 import drawGoogleChart from './libs/drawGoogleChart.js';
-import addToPage from './libs/addToPage.js';
+import makeChartFriendly from './libs/makeChartFriendly.js';
+import * as s from './libs/simple-statistics.js';
 
-let data = await csvLoad('smhi-rainfall-temperature-sthm.csv', ';');
+let data = await jload('./barometer-data.json');
+addToPage(`<h1>Väljarbarometern februari 2025</h1>`);
 
-// Filter: Data from the year 2024
-let data2024 = data.filter(x => x.date >= '2024-01' && x.date <= '2024-12');
-
-// Display a headline
-addToPage('<h2>Nederbörd och temperaturer i Stockholm,<br>månad för månad 2024</h2>');
-
-// Display a table with the data from the data2024 variable
-tableFromData({ data: data2024, columnNames: ['Datum', 'Nederbörd (mm)', 'Temperatur (°C)'] });
-
-// Map: Keep date and temperatureC properties/columns (and not the rainFall column)
-let temperatures2024 = data2024.map(({ date, temperatureC }) => ({ date, temperatureC }));
-
-// Draw a line chart of temperatures2024
-drawGoogleChart({
-  type: 'LineChart',
-  data: makeChartFriendly(temperatures2024, 'Månad', 'Temperatur (°C)'),
-  options: {
-    title: 'Medeltemperaturer i Stockholm 2024, månad för månad',
-    height: 500,
-    curveType: 'function',
-    chartArea: { left: 80 },
-    hAxis: {
-      slantedText: true,
-      slantedAngle: 45
-    }
-  }
+// Add summary of blocks
+// -> empty column space
+data.push({ party: '', percent: 0, color: 'white' });
+// -> sum of M + KD + L + SD
+data.push({
+  party: 'M+KD+L+SD',
+  percent: s.sum(data.filter(x => x.party == 'M' || x.party == 'KD' || x.party == 'L' || x.party == 'SD').map(x => x.percent)),
+  color: "blue"
+});
+// -> sum of  S + V + MP + C
+data.push({
+  party: 'S+V+MP+C',
+  percent: s.sum(data.filter(x => x.party == 'S' || x.party == 'V' || x.party == 'MP' || x.party == 'C').map(x => x.percent)),
+  color: "red"
 });
 
-// Can you make a chart that shows rainfall during 2024 here?
+
+// Add percentages once more as annotations - which shows them above each bar/column
+// Note: The expression with ? and : is a ternary operator! (Shorthand for if-else)
+data = data.map(x => ({ ...x, percentVisible: x.percent ? x.percent.toFixed(1) + '%' : '' }));
+
+// Draw the chart
+drawGoogleChart({
+  type: 'ColumnChart',
+  data: makeChartFriendly(
+    data,
+    "Parti",
+    "Procent",
+    { role: "style" },
+    {
+      type: 'string',
+      role: 'annotation',
+    }
+  ),
+  options: {
+    title: 'Partisympatier',
+    height: 500,
+    // Hide the legend (the small box telling us the unit of the hAxis)
+    legend: { position: 'none' },
+    // set vertical text below columns
+    hAxis: {
+      slantedTextAngle: 90,
+      slantedText: true
+    },
+
+  }
+});
