face3c4a13f9038aab99008fb2623457402cc093
Ironboy <ironboy@users.noreply.github.com>
2025-03-28 04:41:20 +0100

    som extra info

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,7 @@
 import createMenu from './libs/createMenu.js';
 
 createMenu('Malmö-temperaturer', [
+  { name: 'Nytt i version 5', script: 'new-in-v5.js' },
   { name: 'Visa ett år', script: 'one-year.js' },
   { name: 'Jämför två år', script: 'compare-two-years.js' },
   { name: 'Hitta trender', script: 'trends.js' }
