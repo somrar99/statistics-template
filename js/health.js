47e56500b511ab4c6f996e8f01f4e2826adb8e18
Ironboy <ironboy@users.noreply.github.com>
2025-04-03 01:29:29 +0200

    good or bad?

--- js/health.js
+++ js/health.js
@@ -9,44 +9,48 @@ import * as s from './libs/simple-statistics.js';
 // Load the dataset from csv
 let data = await csvLoad('smokinghealth.csv');
 
-console.log('data', data);
-
-// Check distinct/unique values of smoking_status
-// equivalent query in SQL: SELECT DISTINCT smoking_status FROM data
-let smokingStatuses = [...new Set(data.map(x => x.smoking_status))];
-
-console.log('Values of smoking_status', smokingStatuses);
-
-// Check how many of each answer to smoking_status
-// equivalent query in SQL: 
-// SELECT smoking_status, COUNT(*) FROM data GROUP BY smoking_status
-let countAnswers = smokingStatuses.map(
-  status => ({
-    status,
-    count: data.filter(x => x.smoking_status == status).length
-  })
+// Remove outliers (mean +/- std dev * 3) based on general_health
+let health = data.map(x => x.general_health);
+let healthMean = s.mean(health);
+let healthStdDev = s.sampleStandardDeviation(health);
+let outlierLowLimit = healthMean - healthStdDev * 3;
+let outlierHighLimit = healthMean + healthStdDev * 3;
+data = data.filter(x =>
+  x.general_health >= outlierLowLimit && x.general_health <= outlierHighLimit
 );
 
-console.log('Counting answers for smoking_status', countAnswers);
-
 // We will ignore respondents with the answer 0 since it's unclear
+data = data.filter(x => x.smoking_status != 0);
+
+// Draw a histogram for the whole sample (smokers and non-smokers)
+drawGoogleChart({
+  type: 'Histogram',
+  data: makeChartFriendly(
+    data.map(({ general_health }) => ({ general_health })),
+    'General health'
+  ),
+  options: {
+    title: 'General health, all respondents',
+    height: 500,
+    width: 1250,
+    histogram: {
+      bucketSize: 10
+    }
+  }
+});
+
 // if that means 'never smoked' or 'did not answer the question'
 
-// Collecting all non-smokers (never smoked in one starta - nonSmokers)
+// Collecting all non-smokers (never smoked in one strata - nonSmokers)
 let nonSmokers = data.filter(x => x.smoking_status == 'never smoked');
-// Collecting all smokers (previous or current in one starta - smokers)
+// Collecting all smokers (previous or current in one strata - smokers)
 let smokers = data.filter(
   x => x.smoking_status == 'currently smokes' || x.smoking_status == 'previously smoked'
 );
 
-// Check max and min values for general_health
-let healthValues = data.map(x => x.general_health);
-console.log('healthValues', healthValues);
-console.log('General health min value, all respondents', s.min(healthValues));
-console.log('General health max value, all respondents', s.max(healthValues));
-
-console.log('nonSmokers', nonSmokers.length, nonSmokers);
-console.log('smokers', smokers.length, smokers);
+addMdToPage(`
+  **Please Note:** ${nonSmokers.length} non-smokers, but only ${smokers.length} smokers.
+`);
 
 drawGoogleChart({
   type: 'Histogram',
@@ -66,7 +70,11 @@ drawGoogleChart({
 
 let nonSmokerHealthVector = new jerzy.Vector(nonSmokers.map(x => x.general_health));
 let shapiroPValueNonSmokerHealth = jerzy.Normality.shapiroWilk(nonSmokerHealthVector).p;
-addMdToPage(`Shapiro/Wilk-test, P value ${shapiroPValueNonSmokerHealth}`);
+addMdToPage(`
+  Shapiro/Wilk-test, P value ${shapiroPValueNonSmokerHealth}.
+
+  The test indicates that we ${shapiroPValueNonSmokerHealth < 0.05 ? 'do not' : 'do'} have an normal distribution.   
+`);
 
 drawGoogleChart({
   type: 'Histogram',
@@ -84,6 +92,20 @@ drawGoogleChart({
   }
 });
 
-let smokerHealthVector = new jerzy.Vector(smokers.map(x => x.general_health));
+let smokerHealthVector = new jerzy.Vector(smokers.map(x => x.general_health).slice(0, 50));
 let shapiroPValueSmokerHealth = jerzy.Normality.shapiroWilk(smokerHealthVector).p;
-addMdToPage(`Shapiro/Wilk-test, P value ${shapiroPValueSmokerHealth}`);
+addMdToPage(`
+  Shapiro/Wilk-test, P value ${shapiroPValueSmokerHealth}.
+  
+  The test indicates that we ${shapiroPValueSmokerHealth < 0.05 ? 'do not' : 'do'} have an normal distribution.   
+`);
+
+// Why does Shapiro-Wilk give us so low p-values?
+// Thomas will investigate further!
+
+// For now: We decide to proceed with a null hypothesis test,
+// using a two-sample T-test anyway
+
+let pValueTtest = jerzy.StudentT.test(smokerHealthVector, 0.2);
+
+console.log(pValueTtest);
