24808afca2358b345ced0834b355b808e268799e
Ironboy <ironboy@users.noreply.github.com>
2025-04-10 04:55:16 +0200

    exempel kombinera data

--- js/_menu.js
+++ js/_menu.js
@@ -1,4 +1,5 @@
 createMenu('Statistics Template JS', [
   { name: 'Kom igång!', script: 'startpage.js' },
-  { name: 'Övningsinstruktioner', script: 'instructions.js' }
+  { name: 'Övningsinstruktioner', script: 'instructions.js' },
+  { name: 'Medelinkomst per län 2022', script: 'mean-income-by-county-2022.js' }
 ]);
